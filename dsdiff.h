36c6b9c5d41400ce2ad838cc7b27442751b27e89
clone206 <clone206@example.com>
2024-01-21 01:37:48 -0800

    Writing to stdout instead of file. All printed messages to stderr

--- dsdiff.h
+++ dsdiff.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include "dsf.h"
 
-typedef struct {
-    // I think it's OK to hardcode these values because
+typedef struct
+{
+	// I think it's OK to hardcode these values because
 	// a DSF file can only contain so much metadata
 	uint64_t PROPsize;
 	uint64_t DSDsize;
@@ -15,15 +16,14 @@ typedef struct {
 dffinfo init_dffinfo_struct(dsfinfo dsf);
 void dff_channel_conf(char str[], dsfinfo dsf, char hr[]);
 
-void write_header(FILE** outfile, dsfinfo dsf, dffinfo dff, char* channelStr);
-void write_FRM8(FILE** outfile, uint64_t FRM8size);
-void write_FVER(FILE** outfile);
-void write_PROP(FILE** outfile, uint64_t PROPsize);
-void write_FS(FILE** outfile, dsfinfo dsf);
-void write_CHNL(FILE** outfile, dsfinfo dsf, char channelList[]);
-void write_CMPR(FILE** outfile);
-
-void write_DSD(FILE** infile, FILE** outfile, uint64_t DSDsize, dsfinfo dsf);
-void convert_DSD_data(FILE** in, FILE** outfile, dsfinfo dsf);
+void write_header(FILE **outfile, dsfinfo dsf, dffinfo dff, char *channelStr);
+void write_FRM8(FILE **outfile, uint64_t FRM8size);
+void write_FVER(FILE **outfile);
+void write_PROP(FILE **outfile, uint64_t PROPsize);
+void write_FS(FILE **outfile, dsfinfo dsf);
+void write_CHNL(FILE **outfile, dsfinfo dsf, char channelList[]);
+void write_CMPR(FILE **outfile);
+void write_DSD(FILE **infile, dsfinfo dsf);
+void convert_DSD_data(FILE **in, dsfinfo dsf);
 
 #endif
--- dsf2dsdiff.c
+++ dsf2dsdiff.c
@@ -5,47 +5,35 @@
 #include <stdio.h>
 
 // get duration in human-readable form
-void duration_hr(char str[], dsfinfo dsf) {
+void duration_hr(dsfinfo dsf)
+{
 	uint64_t samples = dsf.samples;
 	uint64_t minutes = samples / dsf.fs / 60;
 	samples -= minutes * 60 * dsf.fs;
 	double seconds = (double)samples / dsf.fs;
 
-	snprintf(str, 26, "%02" PRIu64 ":%05.2f", minutes, seconds);
+	fprintf(stderr, "Length: %llu:%05.2f\n", minutes, seconds);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	ASSERT(argv[1], "Error: No input file specified");
-	ASSERT(argv[2], "Error: No output file specified");
-	FILE* infile;
+	FILE *infile;
 	ASSERT(infile = fopen(argv[1], "rb"), "Error: Couldn't open input file");
-	
+
 	check_dsf_headers(&infile); // make sure it's a dsf file
 	dsfinfo dsf = dsfinfo_load(&infile);
 	check_dsf_metadata(dsf); // could help in some cases of broken files
 
-	dffinfo dff = init_dffinfo_struct(dsf);
-	
-	// generate a proper DSDIFF channel list, e.g. "SLFTSRGT" for stereo
-	char channelStr[dsf.channels * 4 + 1];
-	char channelStrHR[7];
-	dff_channel_conf(channelStr, dsf, channelStrHR);
-
-	printf("File %s: ", argv[1]);
-	printf("%s, ", channelStrHR);
+	fprintf(stderr, "\nFile %s: ", argv[1]);
 	double fsMHz = (double)dsf.fs / 1000000; // a bit simpler than printing "DSD64" etc.
-	printf("%lf MHz, ", fsMHz);
+	fprintf(stderr, "%lf MHz, \n", fsMHz);
 	char duration[27] = {0};
-	duration_hr(duration, dsf);
-	puts(duration);
-	
-	printf("Writing to file %s...\n", argv[2]);
-	FILE* outfile;
-	ASSERT(outfile = fopen(argv[2], "wb"), "Error: Couldn't open file for writing");
-	write_header(&outfile, dsf, dff, channelStr);
-	write_DSD(&infile, &outfile, dff.DSDsize, dsf); // do the actual conversion
+	duration_hr(dsf);
+
+	fprintf(stderr, "Writing to stdout...\n\n");
+	write_DSD(&infile, dsf); // do the actual conversion
 
 	fclose(infile);
-	fclose(outfile);
 	return 0;
 }
